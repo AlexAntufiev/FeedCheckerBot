ea8e53aa864c3b1033256418e9ca8e4a2cd6faf8
Semen <semenantufiev@gmail.com>
2020-03-19 18:06:29 +0300

    fix relative path to xmls

--- src/test/kotlin/ru/eda/bot/feedchecker/OfferTest.kt
+++ src/test/kotlin/ru/eda/bot/feedchecker/OfferTest.kt
@@ -8,7 +8,7 @@ class OfferTest {
     @Test
     fun offer_id_null() {
 
-        val readText = this.javaClass.classLoader.getResource("offer_id_null.xml")?.readText()
+        val readText = this.javaClass.classLoader.getResource("offerXMLs/offer_id_null.xml")?.readText()
                 ?: throw IllegalArgumentException("Path not found")
         val validate = FeedValidator().validate(readText)
 
@@ -18,7 +18,7 @@ class OfferTest {
     @Test
     fun offer_id_empty() {
 
-        val readText = this.javaClass.classLoader.getResource("offer_id_empty.xml")?.readText()
+        val readText = this.javaClass.classLoader.getResource("offerXMLs/offer_id_empty.xml")?.readText()
                 ?: throw IllegalArgumentException("Path not found")
         val validate = FeedValidator().validate(readText)
 
@@ -28,7 +28,7 @@ class OfferTest {
     @Test
     fun offer_url_textEmpty() {
 
-        val readText = this.javaClass.classLoader.getResource("offer_url_textEmpty.xml")?.readText()
+        val readText = this.javaClass.classLoader.getResource("offerXMLs/offer_url_textEmpty.xml")?.readText()
                 ?: throw IllegalArgumentException("Path not found")
         val validate = FeedValidator().validate(readText)
 
@@ -38,7 +38,7 @@ class OfferTest {
     @Test
     fun offer_url_null() {
 
-        val readText = this.javaClass.classLoader.getResource("offer_url_null.xml")?.readText()
+        val readText = this.javaClass.classLoader.getResource("offerXMLs/offer_url_null.xml")?.readText()
                 ?: throw IllegalArgumentException("Path not found")
         val validate = FeedValidator().validate(readText)
 
@@ -48,7 +48,7 @@ class OfferTest {
     @Test
     fun offer_categoryId_textEmpty() {
 
-        val readText = this.javaClass.classLoader.getResource("offer_categoryId_textEmpty.xml")?.readText()
+        val readText = this.javaClass.classLoader.getResource("offerXMLs/offer_categoryId_textEmpty.xml")?.readText()
                 ?: throw IllegalArgumentException("Path not found")
         val validate = FeedValidator().validate(readText)
 
@@ -58,7 +58,7 @@ class OfferTest {
     @Test
     fun offer_categoryId_null() {
 
-        val readText = this.javaClass.classLoader.getResource("offer_categoryId_null.xml")?.readText()
+        val readText = this.javaClass.classLoader.getResource("offerXMLs/offer_categoryId_null.xml")?.readText()
                 ?: throw IllegalArgumentException("Path not found")
         val validate = FeedValidator().validate(readText)
 
--- src/test/kotlin/ru/eda/bot/feedchecker/ShopTest.kt
+++ src/test/kotlin/ru/eda/bot/feedchecker/ShopTest.kt
@@ -8,7 +8,7 @@ class ShopTest {
     @Test
     fun shop_name_null() {
 
-        val readText = this.javaClass.classLoader.getResource("shop_name_null.xml")?.readText()
+        val readText = this.javaClass.classLoader.getResource("shopXMLs/shop_name_null.xml")?.readText()
                 ?: throw IllegalArgumentException("Path not found")
         val validate = FeedValidator().validate(readText)
 
@@ -18,7 +18,7 @@ class ShopTest {
     @Test
     fun shop_name_textEmpty() {
 
-        val readText = this.javaClass.classLoader.getResource("shop_name_textEmpty.xml")?.readText()
+        val readText = this.javaClass.classLoader.getResource("shopXMLs/shop_name_textEmpty.xml")?.readText()
                 ?: throw IllegalArgumentException("Path not found")
         val validate = FeedValidator().validate(readText)
 
@@ -28,7 +28,7 @@ class ShopTest {
     @Test
     fun shop_company_null() {
 
-        val readText = this.javaClass.classLoader.getResource("shop_company_null.xml")?.readText()
+        val readText = this.javaClass.classLoader.getResource("shopXMLs/shop_company_null.xml")?.readText()
                 ?: throw IllegalArgumentException("Path not found")
         val validate = FeedValidator().validate(readText)
 
@@ -38,7 +38,7 @@ class ShopTest {
     @Test
     fun shop_company_textEmpty() {
 
-        val readText = this.javaClass.classLoader.getResource("shop_company_textEmpty.xml")?.readText()
+        val readText = this.javaClass.classLoader.getResource("shopXMLs/shop_company_textEmpty.xml")?.readText()
                 ?: throw IllegalArgumentException("Path not found")
         val validate = FeedValidator().validate(readText)
 
@@ -48,7 +48,7 @@ class ShopTest {
     @Test
     fun shop_url_null() {
 
-        val readText = this.javaClass.classLoader.getResource("shop_url_null.xml")?.readText()
+        val readText = this.javaClass.classLoader.getResource("shopXMLs/shop_url_null.xml")?.readText()
                 ?: throw IllegalArgumentException("Path not found")
         val validate = FeedValidator().validate(readText)
 
@@ -58,7 +58,7 @@ class ShopTest {
     @Test
     fun shop_url_textEmpty() {
 
-        val readText = this.javaClass.classLoader.getResource("shop_url_textEmpty.xml")?.readText()
+        val readText = this.javaClass.classLoader.getResource("shopXMLs/shop_url_textEmpty.xml")?.readText()
                 ?: throw IllegalArgumentException("Path not found")
         val validate = FeedValidator().validate(readText)
 
@@ -68,7 +68,7 @@ class ShopTest {
     @Test
     fun shop_currencies_null() {
 
-        val readText = this.javaClass.classLoader.getResource("shop_currencies_null.xml")?.readText()
+        val readText = this.javaClass.classLoader.getResource("shopXMLs/shop_currencies_null.xml")?.readText()
                 ?: throw IllegalArgumentException("Path not found")
         val validate = FeedValidator().validate(readText)
 
@@ -78,7 +78,7 @@ class ShopTest {
     @Test
     fun shop_currencies_textEmpty() {
 
-        val readText = this.javaClass.classLoader.getResource("shop_currencies_textEmpty.xml")?.readText()
+        val readText = this.javaClass.classLoader.getResource("shopXMLs/shop_currencies_textEmpty.xml")?.readText()
                 ?: throw IllegalArgumentException("Path not found")
         val validate = FeedValidator().validate(readText)
 
@@ -88,7 +88,7 @@ class ShopTest {
     @Test
     fun shop_categories_null() {
 
-        val readText = this.javaClass.classLoader.getResource("shop_categories_null.xml")?.readText()
+        val readText = this.javaClass.classLoader.getResource("shopXMLs/shop_categories_null.xml")?.readText()
                 ?: throw IllegalArgumentException("Path not found")
         val validate = FeedValidator().validate(readText)
 
@@ -98,7 +98,7 @@ class ShopTest {
     @Test
     fun shop_categories_textEmpty() {
 
-        val readText = this.javaClass.classLoader.getResource("shop_categories_textEmpty.xml")?.readText()
+        val readText = this.javaClass.classLoader.getResource("shopXMLs/shop_categories_textEmpty.xml")?.readText()
                 ?: throw IllegalArgumentException("Path not found")
         val validate = FeedValidator().validate(readText)
 
@@ -108,7 +108,7 @@ class ShopTest {
     @Test
     fun shop_offers_null() {
 
-        val readText = this.javaClass.classLoader.getResource("shop_offers_null.xml")?.readText()
+        val readText = this.javaClass.classLoader.getResource("shopXMLs/shop_offers_null.xml")?.readText()
                 ?: throw IllegalArgumentException("Path not found")
         val validate = FeedValidator().validate(readText)
 
@@ -118,7 +118,7 @@ class ShopTest {
     @Test
     fun shop_offers_textEmpty() {
 
-        val readText = this.javaClass.classLoader.getResource("shop_offers_textEmpty.xml")?.readText()
+        val readText = this.javaClass.classLoader.getResource("shopXMLs/shop_offers_textEmpty.xml")?.readText()
                 ?: throw IllegalArgumentException("Path not found")
         val validate = FeedValidator().validate(readText)
 
